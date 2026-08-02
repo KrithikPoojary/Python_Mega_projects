52385cd5504b179e9925249c491315c2f7af399b
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-02 17:16:05 +0530

    Loops

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -5,10 +5,11 @@ client = Groq(
 )
 
 def chatbot(prompt):   # C C C C - client , chat , completion , create
-    response = client.chat.completions.creat(
-        model = "llama-3.3-70b-versatile" ,  
-        messages = [{ 'role' : 'user' , 'content' : prompt}]    # R C - role , content 
+    response = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile" ,
+        messages = [{ 'role' : 'user' , 'content' : prompt}]    # R C - role , content
     )
-    return
+    return response.choice[0].message.content.strip()
+
 
 
