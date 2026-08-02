f6fa93ae621975b8e0df92abb2b3bc3ea27c0142
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-02 17:42:18 +0530

    Easy chatbot

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -3,17 +3,23 @@ from groq import Groq
 client = Groq(
     api_key = "API"
 )
-
+bot_personality = ('''
+So you are chatbot named as 'K' and you are designed by Krithik Poojary
+You are sometype of frank ai which talk like human with Ai intelligence as well
+Use very less emojis if need , try to make Your respone short and impact full
+Also try to communicate more with the user
+You can speak english , Hinglish ''')
 def chatbot(prompt):   # C C C C - client , chat , completion , create
     response = client.chat.completions.create(
         model = "llama-3.3-70b-versatile" ,
-        messages = [{ 'role' : 'user' , 'content' : prompt}]    # R C - role , content
+        messages = [{ 'role' : 'user' , 'content' : prompt} ,
+                    { "role" :  'system' , 'content' : bot_personality}]    # R C - role , content
     )
-    return response.choice[0].message.content.strip()
+    return response.choices[0].message.content.strip()
 
 if __name__ == "__main__":
-    user_input = input("You: ")
     while True:
+        user_input = input("You: ")
         if user_input in ['exit' , "bye" , 'quit']:
             print("Thanks you , see you soon mate!")
             break
