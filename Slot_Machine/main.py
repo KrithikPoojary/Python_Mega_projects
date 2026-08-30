131f0bc65a519c9aff36fab0b2e22de3fc0502fb
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-30 13:10:26 +0530

    testing

--- Slot_Machine/main.py
+++ Slot_Machine/main.py
@@ -3,14 +3,14 @@ print("*****Welcome to Slot machine!!!*****")
 def deposit():
     while True:
         amount = input("Enter your deposit: $")   #blud we can do directly but if the value is not int then it gives error (in int(input))
-        if amount.isdigit(): 
+        if amount.isdigit():
             amount= int(amount)
             if amount > 0:
                 break
             else:
                 print("Amount should be greater then 0!")
         else:
-            print("Amount should be Entered as numbers")
+            print("Amount should be entered as numbers")
     return amount
 
 deposit()
