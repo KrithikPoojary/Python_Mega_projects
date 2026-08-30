f3b0bb306b7cef218af3d8fb29bc52f20c450916
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-30 13:05:57 +0530

    add validation for deposit amount and prompt user for input

--- Slot_Machine/main.py
+++ Slot_Machine/main.py
@@ -5,4 +5,8 @@ def deposit():
         amount = int(input("Enter your deposit: $"))
         if amount > 0:
             break
-        
+        else:
+            print("Amount should be greater then 0!")
+    return amount
+
+deposit()
