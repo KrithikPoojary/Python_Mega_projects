1a0203bf0273c2ec770ee8367aade5a66fce0d96
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-30 13:03:57 +0530

    add validation for positive deposit amount

--- Slot_Machine/main.py
+++ Slot_Machine/main.py
@@ -3,4 +3,6 @@ print("*****Welcome to Slot machine!!!*****")
 def deposit():
     while True:
         amount = int(input("Enter your deposit: $"))
+        if amount > 0:
+            break
         
