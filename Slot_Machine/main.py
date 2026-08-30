22b7358671dd7c96f893fcce37ce255afb59ea9a
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-30 13:07:21 +0530

    testing

--- Slot_Machine/main.py
+++ Slot_Machine/main.py
@@ -2,7 +2,7 @@ print("*****Welcome to Slot machine!!!*****")
 
 def deposit():
     while True:
-        amount = int(input("Enter your deposit: $"))
+        amount = input("Enter your deposit: $")
         if amount > 0:
             break
         else:
