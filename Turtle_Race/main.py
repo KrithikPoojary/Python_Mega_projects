2a921fce7629ebcdb394c894f69b8333e292c55a
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-08-31 20:55:15 +0530

    feat: define WIDTH and HEIGHT constants and implement user input function for turtle race

--- Turtle_Race/main.py
+++ Turtle_Race/main.py
@@ -1,1 +1,14 @@
 import turtle
+
+WIDTH , HEIGHT = 500 , 500
+
+
+def user_ask():
+
+    while True:
+
+        user = input('Enter the Number of turtle you want to race (2-10): ')
+        if user.isdigit():
+            user = int(user)
+
+        
