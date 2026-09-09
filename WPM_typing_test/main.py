3c58dfd4e695568cbf4110a710f77d295b59951a
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-09 19:26:24 +0530

    fix: clean up start_screen function and remove commented code in main.py

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -1,7 +1,7 @@
 import curses
 from curses import wrapper
 
-def start_screen(stdscr):
+def start_screen(stdscr):    #Standard screen
     stdscr.clear()
     stdscr.addstr("Welcome to speed typing test!")
     stdscr.addstr("\nPress any key to begin!")
@@ -14,8 +14,10 @@ def main(stdscr):
     curses.init_pair(1 , curses.COLOR_GREEN , curses.COLOR_BLACK)
     curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK)
     curses.init_pair(3 , curses.COLOR_WHITE , curses.COLOR_BLACK)
-    key = stdscr.getkey()
-    print(key)
+    # key = stdscr.getkey()
+    # print(key)
+    start_screen(stdscr)
+
 
 wrapper(main)
 
