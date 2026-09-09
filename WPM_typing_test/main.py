9c5fd280482865df3200ea4865c70decbdd214a7
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-09 19:12:29 +0530

    fix: refactor start_screen function and adjust key handling in main.py

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -1,14 +1,20 @@
 import curses
 from curses import wrapper
 
-def main(stdscr):
-    curses.init_pair(1 , curses.COLOR_GREEN , curses.COLOR_BLACK)
-    curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK) 
-    curses.init_pair(3 , curses.COLOR_WHITE , curses.COLOR_BLACK) 
+def start_screen(stdscr):
     stdscr.clear()
     stdscr.addstr("Hello world")
+    stdscr.addstr("Hello world")
     stdscr.refresh()
-    stdscr.getkey()
+    key = stdscr.getkey()
+    print(key)
+
+
+def main(stdscr):
+    curses.init_pair(1 , curses.COLOR_GREEN , curses.COLOR_BLACK)
+    curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK)
+    curses.init_pair(3 , curses.COLOR_WHITE , curses.COLOR_BLACK)
+
 
 wrapper(main)
 
