1adb947eb2a5d0759c4850b019d3b325406d8abf
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-09 19:24:18 +0530

    fix: update start_screen function to display welcome message and prompt

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -3,18 +3,19 @@ from curses import wrapper
 
 def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello world")
-    stdscr.addstr("Hello world")
+    stdscr.addstr("Welcome to speed typing test!")
+    stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
-    key = stdscr.getkey()
-    print(key)
+    stdscr.getkey()
+    
 
 
 def main(stdscr):
     curses.init_pair(1 , curses.COLOR_GREEN , curses.COLOR_BLACK)
     curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK)
     curses.init_pair(3 , curses.COLOR_WHITE , curses.COLOR_BLACK)
-
+    key = stdscr.getkey()
+    print(key)
 
 wrapper(main)
 
