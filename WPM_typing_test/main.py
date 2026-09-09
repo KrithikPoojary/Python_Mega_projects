3da6b916df037c9241bf38affcff9fe40af8ea30
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-09 18:53:53 +0530

    fix: correct color initialization and update string display in main.py

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -2,10 +2,11 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
-    curses.init_pair(1 , curses.COLOR_RED , curses.COLOR_WHITEl)
-    curses.init_pair(2 , curses.COLOR_YELLOW , curses.COLOR_WHITE) 
+    curses.init_pair(1 , curses.COLOR_GREEN , curses.COLOR_BLACK)
+    curses.init_pair(2 , curses.COLOR_RED , curses.COLOR_BLACK) 
+    curses.init_pair(3 , curses.COLOR_WHITE , curses.COLOR_BLACK) 
     stdscr.clear()
-    stdscr.addstr("Hello world" , curses.color_pair(1))
+    stdscr.addstr("Hello world")
     stdscr.refresh()
     stdscr.getkey()
 
