a457aa2e08dc708dafe78c49efe441e2b63b92ba
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-09 18:41:42 +0530

    fix: correct color initialization and remove trailing comment in main.py

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -2,6 +2,7 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1 , curses.COLOR_RED , curses.COLOR_WHITEl)
     stdscr.clear()
     stdscr.addstr("Hello world")
     stdscr.refresh()
@@ -9,4 +10,3 @@ def main(stdscr):
 
 wrapper(main)
 
-# working..
