90da3518cddcda6c8372f3a6bafd8461d8b9b8cf
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-13 00:18:10 +0530

    fix: add condition to handle incorrect characters in display_test function

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -13,6 +13,8 @@ def display_test(stdscr , target, current , wpm=0):
     for  i , char in  enumerate(current):
         correct_char = target[i]
         curses.color_pair(1)
+        if char != correct_char:
+            k
         stdscr.addstr(0 , i , char , )  # i will represent at which index will it be placed on..
 #(0 , i)  will be the logic which will be overlayed on top of the current test
 
