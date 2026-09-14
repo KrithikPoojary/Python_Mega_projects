0625f2eb41f2d0000c3df4ddc053cbb2b49306ca
KrithikPoojary <krithikpoojary2006@gmail.com>
2026-09-14 11:49:56 +0530

    fix: complete conditional statement for character input in wpm_work function

--- WPM_typing_test/main.py
+++ WPM_typing_test/main.py
@@ -36,7 +36,7 @@ def wpm_work(stdscr):
         if key in ("KEY_BACKSPACE" , "\b" , "\x7f"):  #This is basically our 'backspace' key value in our OS
             if len(current_test) > 0:
                 current_test.pop()
-        elif 
+        elif len(current_test) < len(Target_test):
             current_test.append(key)
 
 
